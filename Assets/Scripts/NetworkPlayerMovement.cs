4b600446cd559fdcae9e38b3ca306026a1e100ad
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-02-22 16:10:57 -0800

    Change fonts, change player model, add ground check for jumping

--- Assets/Scripts/NetworkPlayerMovement.cs
+++ Assets/Scripts/NetworkPlayerMovement.cs
@@ -9,6 +9,11 @@ public class NetworkPlayerMovement : NetworkBehaviour
     [SerializeField] private new Camera camera;
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private float lookSpeed = 10f;
+    [SerializeField] private ColliderEventDispatcher groundChecker;
+
+    private Vector3 _lastDirection;
+    private bool _canJump;
 
     private void Awake()
     {
@@ -28,17 +33,35 @@ public class NetworkPlayerMovement : NetworkBehaviour
         var axis = PlayerControls.Instance.Actions.Player.Move.ReadValue<Vector2>();
         var direction = forward * axis.y + right * axis.x;
 
+        if (PlayerControls.Instance.Actions.Player.Move.IsPressed())
+            _lastDirection = direction;
+
         Move(direction);
+        Look(_lastDirection);
     }
 
     private void OnEnable()
     {
         PlayerControls.Instance.Actions.Player.Jump.performed += Jump;
+        groundChecker.OnTriggerEntered += OnGroundCheckerEnter;
+        groundChecker.OnTriggerExited += OnGroundCheckerExit;
     }
 
     private void OnDisable()
     {
         PlayerControls.Instance.Actions.Player.Jump.performed -= Jump;
+        groundChecker.OnTriggerEntered -= OnGroundCheckerEnter;
+        groundChecker.OnTriggerExited -= OnGroundCheckerExit;
+    }
+
+    private void OnGroundCheckerEnter(Collider other)
+    {
+        _canJump = true;
+    }
+
+    private void OnGroundCheckerExit(Collider other)
+    {
+        _canJump = false;
     }
 
     private void Move(Vector3 direction)
@@ -56,9 +79,19 @@ public class NetworkPlayerMovement : NetworkBehaviour
     private void Jump(InputAction.CallbackContext ctx)
     {
         if (!IsOwner) return;
+        if (!_canJump) return;
+        _canJump = false;
+        
+        print("jumped");
 
         var velocity = rigidbody.linearVelocity;
         velocity.y = jumpSpeed;
         rigidbody.linearVelocity = velocity;
+    }
+
+    private void Look(Vector3 direction)
+    {
+        var rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * lookSpeed);
     }
 }
