3b41fd0e21661b1b464f57622c571b5cef310e6b
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2025-03-01 12:19:27 -0800

    Optimize name tag rotation calculations

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -13,8 +13,8 @@ public class Billboard : MonoBehaviour
     private void Update()
     {
         transform.LookAt(
-            transform.position + mainCamera.transform.rotation * Vector3.forward, 
-            mainCamera.transform.rotation * Vector3.up
+            transform.position + mainCamera.transform.forward, 
+            mainCamera.transform.up
         );
     }
 }
